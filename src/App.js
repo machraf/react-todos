1db8e8b5941b6f0d3f86c78eb61f7dee0d020556
machraf <mohamed.achraf.assadiki@gmail.com>
2020-11-08 16:19:36 +0100

    add todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "", todos: [] };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.setState({ todos: [...this.state.todos, this.state.value] });
+    event.preventDefault();
+  }
+  render() {
+    const todos = this.state.todos.map((todo, index) => (
+      <li key={index}>{todo}</li>
+    ));
+    return (
+      <div className="App">
+        <h1>My Todos</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Add" />
+        </form>
+        <ul className="todos">{todos}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
