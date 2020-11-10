f2155b118f2d6247566486c7bcd1cf05807aa013
machraf <mohamed.achraf.assadiki@gmail.com>
2020-11-10 19:31:55 +0100

    Added delete functionality

--- src/App.js
+++ src/App.js
@@ -7,20 +7,36 @@ class App extends React.Component {
     super(props);
     this.state = { value: "", todos: [] };
 
+    this.counter = 0;
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
   }
   handleChange(event) {
     this.setState({ value: event.target.value });
   }
 
   handleSubmit(event) {
-    this.setState({ todos: [...this.state.todos, this.state.value] });
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        { id: this.counter++, name: this.state.value },
+      ],
+    });
     event.preventDefault();
   }
+  deleteTodo(todo) {
+    const id = this.state.todos.indexOf(todo);
+    const todos = this.state.todos;
+    todos.splice(id, 1);
+    this.setState({ todos: todos });
+  }
   render() {
-    const todos = this.state.todos.map((todo, index) => (
-      <li key={index}>{todo}</li>
+    const todos = this.state.todos.map((todo) => (
+      <li key={todo.id}>
+        {todo.name}
+        <span onClick={() => this.deleteTodo(todo)}>&times;</span>
+      </li>
     ));
     return (
       <div className="App">
